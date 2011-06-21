0a23452aa1abe87ccd8235f2311a4512b62042d0
mehlis <mehlis@ec5dfc5b-5239-0410-a031-e37cc464ff32>
2011-06-21 19:47:02 +0000

    add parenthesis around defines
    git-svn-id: https://svn.mi.fu-berlin.de/des-testbed/Software/DES-SERT/des-aodv/trunk@7886 ec5dfc5b-5239-0410-a031-e37cc464ff32

--- src/pipeline/aodv_pipeline.h
+++ src/pipeline/aodv_pipeline.h
@@ -37,21 +37,21 @@ extern uint32_t broadcast_id;
 /**
  * Unknown sequence number
  */
-#define AODV_FLAGS_RREQ_U 			1 << 11
+#define AODV_FLAGS_RREQ_U 			(1 << 11)
 
 /**
  * Destination only flag
  */
-#define AODV_FLAGS_RREQ_D			1 << 12
+#define AODV_FLAGS_RREQ_D			(1 << 12)
 /**
  * Aknowledgement required
  */
-#define AODV_FLAGS_RREP_A			1 << 6
+#define AODV_FLAGS_RREP_A			(1 << 6)
 
 /**
  * Not delete flag of RERR
  */
-#define AODV_FLAGS_RERR_N			1 << 7
+#define AODV_FLAGS_RERR_N			(1 << 7)
 
 /** RREQ - Route Request Message */
 struct aodv_msg_rreq {

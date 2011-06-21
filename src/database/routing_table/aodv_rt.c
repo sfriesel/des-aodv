6a8574db5c691d6e761439c9ea63ac47b2c102cf
mehlis <mehlis@ec5dfc5b-5239-0410-a031-e37cc464ff32>
2011-06-21 19:57:33 +0000

    revert rt and add readable ifs
    git-svn-id: https://svn.mi.fu-berlin.de/des-testbed/Software/DES-SERT/des-aodv/trunk@7894 ec5dfc5b-5239-0410-a031-e37cc464ff32

--- src/database/routing_table/aodv_rt.c
+++ src/database/routing_table/aodv_rt.c
@@ -167,7 +167,6 @@ int nht_entry_create (nht_entry_t** entry_out, uint8_t dhost_next_hop[ETH_ALEN])
 
 //returns TRUE if input entry is used (newer)
 //        FALSE if input entry is unused
-//        -1 if error
 int aodv_db_rt_capt_rreq (uint8_t dhost_ether[ETH_ALEN], uint8_t shost_ether[ETH_ALEN],
 		uint8_t shost_prev_hop[ETH_ALEN], dessert_meshif_t* output_iface,
 		uint32_t shost_seq_num, struct timeval* timestamp) {
@@ -178,8 +177,8 @@ int aodv_db_rt_capt_rreq (uint8_t dhost_ether[ETH_ALEN], uint8_t shost_ether[ETH
 	HASH_FIND(hh, rt.entrys, dhost_ether, ETH_ALEN, rt_entry);
 	if (rt_entry == NULL) {
 		// if not found -> create routing entry
-		if (rt_entry_create(&rt_entry, dhost_ether) == FALSE) {
-			return -1;
+		if (rt_entry_create(&rt_entry, dhost_ether) != TRUE) {
+			return FALSE;
 		}
 		HASH_ADD_KEYPTR(hh, rt.entrys, rt_entry->dhost_ether, ETH_ALEN, rt_entry);
 	}
@@ -188,13 +187,21 @@ int aodv_db_rt_capt_rreq (uint8_t dhost_ether[ETH_ALEN], uint8_t shost_ether[ETH
 	if (srclist_entry == NULL) {
 		// if not found -> create new source entry of source list
 		if (rt_srclist_entry_create(&srclist_entry, shost_ether, shost_prev_hop,
-				output_iface, shost_seq_num) == FALSE) {
-			return -1;
+				output_iface, shost_seq_num) != TRUE) {
+			return FALSE;
 		}
 		HASH_ADD_KEYPTR(hh, rt_entry->src_list, srclist_entry->shost_ether, ETH_ALEN, srclist_entry);
 		timeslot_addobject(rt.ts, timestamp, rt_entry);
+		dessert_debug("create route to " MAC ": shost_seq_num=%d",
+		              EXPLODE_ARRAY6(shost_ether), srclist_entry->shost_seq_num);
 		return TRUE;
-	} else if (hf_seq_comp_i_j(srclist_entry->shost_seq_num, shost_seq_num) < 0) {
+	}
+
+	int a = hf_seq_comp_i_j(srclist_entry->shost_seq_num, shost_seq_num);
+	if(a < 0) {
+		dessert_debug("get " MAC ": shost_seq_num=%d:%d",
+		              EXPLODE_ARRAY6(shost_ether), srclist_entry->shost_seq_num, shost_seq_num);
+
 		// overwrite several fields of source entry if source seq_num is newer
 		memcpy(srclist_entry->shost_prev_hop, shost_prev_hop, ETH_ALEN);
 		srclist_entry->output_iface = output_iface;
@@ -207,7 +214,6 @@ int aodv_db_rt_capt_rreq (uint8_t dhost_ether[ETH_ALEN], uint8_t shost_ether[ETH
 
 // returns TRUE if rep is newer
 //         FALSE if rep is discarded
-//         -1 error
 int aodv_db_rt_capt_rrep (uint8_t dhost_ether[ETH_ALEN], uint8_t dhost_next_hop[ETH_ALEN],
 		dessert_meshif_t* output_iface, uint32_t dhost_seq_num, uint8_t hop_count, struct timeval* timestamp) {
 	aodv_rt_entry_t* rt_entry;
@@ -215,13 +221,15 @@ int aodv_db_rt_capt_rrep (uint8_t dhost_ether[ETH_ALEN], uint8_t dhost_next_hop[
 	if (rt_entry == NULL) {
 		// if not found -> create routing entry
 		if (rt_entry_create(&rt_entry, dhost_ether) == FALSE) {
-			return -1;
+			return FALSE;
 		}
 		HASH_ADD_KEYPTR(hh, rt.entrys, rt_entry->dhost_ether, ETH_ALEN, rt_entry);
 	}
-	if ((rt_entry->flags & AODV_FLAGS_NEXT_HOP_UNKNOWN) ||
-			(hf_seq_comp_i_j(dhost_seq_num, rt_entry->dhost_seq_num) > 0) ||
-			((dhost_seq_num == rt_entry->dhost_seq_num) && (hop_count < rt_entry->hop_count))) {
+	int a = (rt_entry->flags & AODV_FLAGS_NEXT_HOP_UNKNOWN);
+	int b = (hf_seq_comp_i_j(rt_entry->dhost_seq_num, dhost_seq_num));
+	int c = (rt_entry->hop_count > hop_count);
+
+	if(a || (b < 0) || (b == 0 && c)) {
 
 		nht_entry_t* nht_entry;
 		nht_destlist_entry_t* destlist_entry;

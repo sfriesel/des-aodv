57a5ffa664010aef605bc09f9e642a2afe97fba8
Stefan Friesel <stefan.friesel@googlemail.com>
2012-04-17 20:28:42 +0200

    fixup

--- src/database/aodv_database.c
+++ src/database/aodv_database.c
@@ -163,9 +163,9 @@ int aodv_db_capt_rreq(mac_addr destination_host, mac_addr originator_host, mac_a
     return result;
 }
 
-int aodv_db_capt_rrep(mac_addr destination_host, mac_addr next_hop, dessert_meshif_t* output_iface, uint32_t destination_sequence_number, metric_t metric, uint8_t hop_count, struct timeval const *timestamp) {
+int aodv_db_capt_rrep(mac_addr destination_host, mac_addr prev_hop, dessert_meshif_t* output_iface, uint32_t destination_sequence_number, metric_t metric, uint8_t hop_count, struct timeval const *timestamp) {
     aodv_db_wlock();
-    int result =  aodv_db_rt_capt_rrep(destination_host, next_hop, output_iface, destination_sequence_number, metric, hop_count, timestamp);
+    int result =  aodv_db_rt_capt_rrep(destination_host, prev_hop, output_iface, destination_sequence_number, metric, hop_count, timestamp);
     aodv_db_unlock();
     return result;
 }
